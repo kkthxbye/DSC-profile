3f7fa4994408c67ba20a63d4f387b518582817d7
kkthxbye <kkthxbye@users.noreply.github.com>
2026-08-20 10:37:48 +0200

    Fix the lockscreen

---
+++ configs/wallpaper/Set-LockScreenImage.ps1
@@ -0,0 +1,34 @@
+$imagePath = "C:\code\DSC-profile\configs\wallpaper\lockscreen.jpg"
+
+if ($PSVersionTable.PSEdition -ne 'Desktop') {
+    powershell.exe -NoLogo -NonInteractive -NoProfile -File $PSCommandPath
+    exit $LASTEXITCODE
+}
+
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+
+$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object {
+    $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1'
+})[0]
+$asTaskAction = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where-Object {
+    $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncAction'
+})[0]
+
+function Await($WinRtTask, $ResultType) {
+    $netTask = $asTaskGeneric.MakeGenericMethod($ResultType).Invoke($null, @($WinRtTask))
+    $netTask.Wait(-1) | Out-Null
+    $netTask.Result
+}
+
+function AwaitAction($WinRtAction) {
+    $netTask = $asTaskAction.Invoke($null, @($WinRtAction))
+    $netTask.Wait(-1) | Out-Null
+}
+
+[Windows.Storage.StorageFile, Windows.Storage, ContentType = WindowsRuntime] | Out-Null
+[Windows.System.UserProfile.LockScreen, Windows.System.UserProfile, ContentType = WindowsRuntime] | Out-Null
+
+$file = Await ([Windows.Storage.StorageFile]::GetFileFromPathAsync($imagePath)) ([Windows.Storage.StorageFile])
+AwaitAction ([Windows.System.UserProfile.LockScreen]::SetImageFileAsync($file))
+
+Write-Host "Lock screen image set to $imagePath"
